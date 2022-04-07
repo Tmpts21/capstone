626f98670344e52dd4785ae7bf3f7feff6313e6a
mivatampos@tip.edu.ph <mivatampos@tip.edu.ph>
2022-04-08 00:21:53 +0800

    fix some links in mobile view

--- resources/js/Layouts/Authenticated.vue
+++ resources/js/Layouts/Authenticated.vue
@@ -111,14 +111,16 @@ const showingNavigationDropdown = ref(false);
 
                 <!-- Responsive Navigation Menu -->
                 <div :class="{'block': showingNavigationDropdown, 'hidden': ! showingNavigationDropdown}" class="sm:hidden">
-                    <div class="pt-2 pb-3 space-y-1">
-                        <BreezeResponsiveNavLink class="ml-3 bg-blue-800 text-white rounded-lg " :href="route('dashboard')" :active="route().current('dashboard')">
-                            Dashboard
-                        </BreezeResponsiveNavLink>
-                    </div>
+
 
                     <!-- Responsive Settings Options -->
                     <div class="pt-4 pb-1 border-t border-gray-200">
+
+                        <div class="pt-2 pb-3 space-y-1">
+                            <BreezeResponsiveNavLink class="ml-3 bg-blue-800 text-white rounded-lg" :href="route('dashboard')" :active="route().current('dashboard')">
+                                Dashboard
+                            </BreezeResponsiveNavLink>
+                        </div>
 
                         <div class="mt-3 space-y-1">
                              <BreezeResponsiveNavLink class="ml-3 bg-blue-800 text-white rounded-lg" :href="route('profile')" method="get" as="button">
