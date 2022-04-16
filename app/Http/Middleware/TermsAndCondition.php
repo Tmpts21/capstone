d02325bfaff1d26b7f197cd32e1e80f9031227a2
mivatampos@tip.edu.ph <mivatampos@tip.edu.ph>
2022-04-16 15:11:49 +0800

    testing middlware

--- app/Http/Middleware/TermsAndCondition.php
+++ app/Http/Middleware/TermsAndCondition.php
@@ -19,10 +19,10 @@ class TermsAndCondition
     public function handle(Request $request, Closure $next)
     {
 
-        if (Auth::user() &&  Auth::user()->data_privacy == 1) {
-            return $next($request);
+        if (Auth::user() &&  Auth::user()->data_privacy == 0) {
+            return Inertia::render('DataPrivacy');
         }
 
-        return Inertia::render('DataPrivacy');
+        return $next($request);
     }
 }
